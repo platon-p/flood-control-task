1da24b511881fd3f5380b6dfd7ba44ffff3a0a3b
platon <platonpechenev@ya.ru>
2024-03-19 23:15:23 +0300

    flood control impl

--- control.go
+++ control.go
@@ -1,10 +1,33 @@
 package main
 
-import "context"
+import (
+	"context"
+	"time"
+)
 
 type FloodControlImpl struct {
+	allowedPeriod   time.Duration
+	allowedAttempts int
+	storage         AttemptsStorage
 }
 
-func (c *FloodControlImpl) Check(context context.Context, userID int64) (bool, error) {
-    panic("not implemented")
+type AttemptsStorage interface {
+	GetAttemptsCount(ctx context.Context, userID int64, period time.Duration) (int, error)
+	RegisterAttempt(ctx context.Context, userID int64, time time.Time) error
+}
+
+func (c *FloodControlImpl) Check(ctx context.Context, userID int64) (bool, error) {
+	attempts, err := c.storage.GetAttemptsCount(ctx, userID, c.allowedPeriod)
+	if err != nil {
+		return false, err
+	}
+	if attempts >= c.allowedAttempts {
+		return false, nil
+	}
+	// grant access
+	err = c.storage.RegisterAttempt(ctx, userID, time.Now())
+	if err != nil {
+		return false, err
+	}
+	return true, nil
 }
