954eb1243365b405eeee5712c610b5e677d70a1d
platon <platonpechenev@ya.ru>
2024-03-19 23:57:54 +0300

    storage tests

---
+++ storage_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+	"testing"
+	"time"
+)
+
+func TestStorage_GetAttemptsCount(t *testing.T) {
+	storage := NewAttemptsMemoryStorage()
+	userID := int64(1)
+	_, err := storage.GetAttemptsCount(context.Background(), userID, time.Minute)
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+}
+
+func TestStorage_RegisterAttempt(t *testing.T) {
+	storage := NewAttemptsMemoryStorage()
+	userID := int64(1)
+	err := storage.RegisterAttempt(context.Background(), userID, time.Now())
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+}
